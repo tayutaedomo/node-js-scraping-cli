43e980292259f133eb1cedf40095779e86ca4375
tayutaedomo <tayutaedomo@gmail.com>
2015-11-01 05:48:49 +0900

    Refactoring of get_syosetu_novel_top.js

---
+++ app/novel_top.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var client = require('cheerio-httpcli');
+
+var NovelTop = function() {
+    return {
+        get: function(url, callback) {
+            var novel = {};
+
+            client.fetch(url)
+                .then(function (result) {
+                    novel = extract_novel_top(result.$);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    callback(novel);
+                });
+        }
+    };
+
+    function extract_novel_top($) {
+        var data = {};
+
+        data['title'] = $('.novel_title').text();
+        data['author'] = $('.novel_writername a').text();
+        var matches = $('.novel_writername a').attr('href').match(/(\d+)/);
+        data['author'] = matches[1];
+        data['outline'] = $('#novel_ex').text();
+
+        var links = [];
+        $('.subtitle a').each(function(index, element){
+            links.push({
+                title: $(element).text(),
+                url: $(element).attr('href')
+            });
+        });
+
+        data['links'] = links;
+
+        return data;
+    }
+};
+
+
+if (require.main === module) {
+    var url = "http://ncode.syosetu.com/n6316bn/";
+
+    var novelTop = new NovelTop();
+    novelTop.get(url, function(data) {
+        console.log(data);
+    });
+}
+
