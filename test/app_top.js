9d5dd2c7bcd7dd1c0db11ca167e8d41456775461
tayutaedomo <tayutaedomo@gmail.com>
2015-11-01 17:10:45 +0900

    Add mock logic to test/app_top.js

--- test/app_top.js
+++ test/app_top.js
@@ -1,6 +1,19 @@
 var assert = require('assert');
 var Top = require('./../app/top.js');
 
+var nock = require('nock');
+nock('http://syosetu.com').get('/').replyWithFile(200, __dirname + '/fixtures/syosetu.com.html');
+
+//var nockBack = require('nock').back;
+//nockBack.fixtures = __dirname + '/fixtures';
+//nockBack.setMode('lockdown');
+//nockBack.setMode('record');
+//nockBack('syosetu.com.json', function(nockDone) {
+//    request.get('http://syosetu.com/', function(err, res, body) {
+//        nockDone();
+//    });
+//});
+
 describe('Top', function() {
     var top;
 
